295100c13bcc2e37f52afd1e076f7298905682a1
John Easter <john@owltestprep.com>
2020-07-11 16:15:27 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -10,7 +10,11 @@ def fourth_challenge
           :age => "17",
           :attitude => "worried"
         },
-        {:
+        {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }
       ]
    }, 
    :capulet => {
@@ -18,7 +22,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }
       ]
    }
 }
